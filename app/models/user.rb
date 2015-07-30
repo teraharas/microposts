0d2865babcaaff2d889427d07a47536b54b8f640
teraharas <triple.zero.option@gmail.com>
2015-07-30 15:23:46 +0000

    add followings/followers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,49 @@
 class User < ActiveRecord::Base
+  
   before_save { self.email = email.downcase }
+  
   validates :name, presence: true, length: { maximum: 50 }
+  
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+                    
   has_secure_password
+  
   
   # ユーザーとつぶやきを定義  User：microposts = 1：n
   has_many :microposts
+
+
+  has_many :following_relationships, class_name:  "Relationship",
+                                     foreign_key: "follower_id",
+                                     dependent:   :destroy
+  has_many :following_users, through: :following_relationships, source: :followed
+
+
+
+  has_many :followed_relationships, class_name:  "Relationship",
+                                    foreign_key: "followed_id",
+                                    dependent:   :destroy
+  has_many :followed_users, through: :followed_relationships, source: :follower
+  
+  
+  
+  
+  # 他のユーザーをフォローする
+  def follow(other_user)
+    following_relationships.create(followed_id: other_user.id)
+  end
+
+  # フォローしているユーザーをアンフォローする
+  def unfollow(other_user)
+    following_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # あるユーザーをフォローしているかどうか？
+  def following?(other_user)
+    following_users.include?(other_user)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,6 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   
   resources :microposts
+  
+  resources :relationships, only: [:create, :destroy]
 end
