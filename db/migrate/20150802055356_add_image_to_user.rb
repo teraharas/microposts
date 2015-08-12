6fbfdfd80fd0afca6a405a17a6cb8d969f91da84
teraharas <triple.zero.option@gmail.com>
2015-08-12 23:26:41 +0000

    Text

--- db/migrate/20150802055356_add_image_to_user.rb
+++ db/migrate/20150802055356_add_image_to_user.rb
@@ -1,5 +1,5 @@
 class AddImageToUser < ActiveRecord::Migration
   def change
-    add_column :users, :image, :file
+    add_column :users, :image, :string
   end
 end
--- db/migrate/20150802132045_add_image_to_micropost.rb
+++ db/migrate/20150802132045_add_image_to_micropost.rb
@@ -1,6 +1,6 @@
 class AddImageToMicropost < ActiveRecord::Migration
   def change
-    add_column :microposts, :image, :file
+    add_column :microposts, :image, :string
     add_column :microposts, :remove_image, :boolean
     add_column :microposts, :image_cache, :string
   end
